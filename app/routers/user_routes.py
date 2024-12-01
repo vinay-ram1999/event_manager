86845b7a05cfff30c8481974e2227405fd523c63
Vinay Ram Gazula <vinayramgazula@gmail.com>
2024-12-01 00:06:08 -0500

    fixed nicknames; pytest executed;

--- app/routers/user_routes.py
+++ app/routers/user_routes.py
@@ -33,9 +33,13 @@ from app.services.jwt_service import create_access_token
 from app.utils.link_generation import create_user_links, generate_pagination_links
 from app.dependencies import get_settings
 from app.services.email_service import EmailService
+
 router = APIRouter()
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 settings = get_settings()
+
 @router.get("/users/{user_id}", response_model=UserResponse, name="get_user", tags=["User Management Requires (Admin or Manager Roles)"])
 async def get_user(user_id: UUID, request: Request, db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme), current_user: dict = Depends(require_role(["ADMIN", "MANAGER"]))):
     """
